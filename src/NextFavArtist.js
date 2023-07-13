0840f227f2080d4c4b123b325b75e122fa830d6e
MadBeignet <maddie.wisinski@ymail.com>
2023-07-12 19:30:59 -0500

    removed unused import

--- src/NextFavArtist.js
+++ src/NextFavArtist.js
@@ -1,5 +1,5 @@
 import "./NextFavArtist.css";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import spotify_logo from "./Spotify_Logo_CMYK_White.png";
 
